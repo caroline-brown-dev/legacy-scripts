a07cf54ff350acad42dd6c57a24c8ac73a4736a7
Caroline <cline.larval-0r@icloud.com>
2024-11-21 15:52:57 +0200

    Update aws_creds.py

--- scripts/aws_creds.py
+++ scripts/aws_creds.py
@@ -1,4 +1,4 @@
 # AWS Credentials - DO NOT SHARE!!!
-aws_access_key_id = "FLAG{OSINT_LEVEL_1}"
-aws_secret_access_key = "THIS_IS_NOT_A_REAL_KEY"
+aws_access_key_id = "THIS_IS_NOT_A_REAL_KEY1"
+aws_secret_access_key = "THIS_IS_NOT_A_REAL_KEY2"
 region = "us-west-2"
